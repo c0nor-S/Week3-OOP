7b88dfa9f19843651a89625011f331b1c6052a40
CONORSHAUGHNESSY-STU <G00432599@atu.ie>
2024-10-02 16:53:02 +0100

    Added new method called 'add'

---
+++ src/main/java/ie/atu/week3/Calculator.java
@@ -0,0 +1,26 @@
+package ie.atu.week3;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        //methods
+        System.out.println("Please Enter Your First Number: ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("Please Enter Your Second Number: ");
+        int b = sc.nextInt();
+        System.out.println("Please Enter An Operation: ");
+        String op = sc.next();
+        switch (op) {
+            case "add": add(a, b); break;
+            default: System.out.println("Invalid Operation"); break;
+        }
+
+    }
+    public static void add(int a, int b)
+    {
+        int sum = a + b;
+        System.out.println("The Sum Is: " + sum);
+    }
+}
